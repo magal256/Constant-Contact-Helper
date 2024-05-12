96ab2e6b502743ff9866e72ff45383210b21328c
Maksim Galichenko <maksim.galichenko@waveaccess.global>
2024-05-13 00:21:19 +0300

    feat (advertiser): put first function into api  (#1)

--- src/main/java/org/constantcontact/advertiser/controller/AdvertiserController.java
+++ src/main/java/org/constantcontact/advertiser/controller/AdvertiserController.java
@@ -1,17 +1,33 @@
 package org.constantcontact.advertiser.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.constantcontact.advertiser.dto.ContactInfoDto;
+import org.constantcontact.advertiser.model.ItemToSendBuilder;
+import org.constantcontact.advertiser.service.v2.ConstantContactSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.constantcontact.advertiser.model.ConstantContactWorkList.BusinessList;
 
 @RestController
 @RequestMapping("${spring.application.apiPath}/advertiser/constant-contact")
 @Tag(name = "Advertiser web API")
 public class AdvertiserController {
 
+    @Autowired
+    private ConstantContactSender sender;
+
     @PostMapping(path = "/save-contact")
-    public ResponseEntity<String> saveContact(final @RequestBody String email) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(email);
+    public ResponseEntity<String> saveContact(final @RequestBody ContactInfoDto contact) {
+        sender.addEmailToQueue(
+                ItemToSendBuilder.newBuilder()
+                        .email(contact.getEmail())
+                        .list(BusinessList)
+                        .site(contact.getAccount())
+                        .build()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body("Message has been added to queue");
     }
 }
---
+++ src/main/java/org/constantcontact/advertiser/dto/ContactInfoDto.java
@@ -0,0 +1,9 @@
+package org.constantcontact.advertiser.dto;
+
+import lombok.Data;
+
+@Data
+public class ContactInfoDto {
+    private String email;
+    private String account;
+}
